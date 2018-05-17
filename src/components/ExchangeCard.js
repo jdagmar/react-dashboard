8ed9a1ff73708d171d6f2bf4dd3b9a6e6aef697f
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-05-17 16:31:02 +0200

    added icons

--- src/components/ExchangeCard.js
+++ src/components/ExchangeCard.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import EuroIcon from '../icons/ecommerce_euro.svg';
+import RefreshIcon from '../icons/arrows_rotate.svg';
 
-function ExchangeCard(){
+function ExchangeCard() {
     return (
         <div className="border border-grey p-2">
             <p>10 sek</p>
-            <p>1 euro</p>
+            <p>1 euro <img src={EuroIcon}/></p>
+            <button>
+                Refresh
+            <img src={RefreshIcon} />
+            </button>
         </div>
     );
 }
--- src/components/MemoCard.js
+++ src/components/MemoCard.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import SaveIcon from '../icons/basic_floppydisk.svg';
+import EditIcon from '../icons/software_pencil.svg';
+import DeleteIcon from '../icons/basic_trashcan.svg'
 
 function MemoCard(){
     return (
         <div className="border border-grey p-2">
             <textarea className="block" rows="4" cols="50"></textarea>
-            <button>Save</button>
-            <button>Edit</button>
-            <button>Delete</button>
+            <button>
+                Save
+                <img src={SaveIcon}/>
+            </button>
+            <button>
+                Edit
+                <img src={EditIcon} />
+            </button>
+            <button>
+                Delete
+                <img src={DeleteIcon} />
+            </button>
         </div>
     );
 }
