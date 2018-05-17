54cd9beea68f87aec088c2d5938ee8e81a351534
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-05-17 22:27:23 +0200

    made memocard stateless and moved state to app

--- src/App.js
+++ src/App.js
@@ -4,24 +4,39 @@ import Container from './components/Container';
 import ExchangeCard from './components/ExchangeCard';
 import TimerCard from './components/TimerCard';
 import ClockCard from './components/ClockCard';
-import MemoCard from './components/MemoCard';
+import { MemoCard } from './components/MemoCard';
 import WeatherCard from './components/WeatherCard';
 import { GetWeather } from './components/GetWeather';
 
 class App extends Component {
-  render() {
-    return (
-      <Wrapper>
-        <ExchangeCard />
-        <ClockCard />
-        <GetWeather />
-        <Container>
-          <TimerCard />
-          <MemoCard />
-        </Container>
-      </Wrapper>
-    )
-  }
+
+	state = {
+		memo: {
+			editMode: false,
+			memoText: localStorage.getItem('memotext') || ''
+		}
+	}
+
+	render() {
+		return (
+			<Wrapper>
+				<ExchangeCard />
+				<ClockCard />
+				<GetWeather />
+				<Container>
+					<TimerCard />
+					<MemoCard
+						editMode={this.state.memo.editMode}
+						memoText={this.state.memo.memoText}
+						onEdit={() => this.setState({ memo: { editMode: true } })}
+						onSave={() => this.setState({ memo:  { editMode: false } })}
+            			onCancel={() => this.setState({ memo: { editMode: false } })}
+						onUpdate={(memoText) => this.setState({ memo: { memoText } })}
+					/>
+        		</Container>
+			</Wrapper>
+		)
+	}
 }
 
 export default App;
--- src/components/MemoCard.js
+++ src/components/MemoCard.js
@@ -2,27 +2,39 @@ import React, { Component } from 'react';
 import SaveIcon from '../icons/basic_floppydisk.svg';
 import EditIcon from '../icons/software_pencil.svg';
 import DeleteIcon from '../icons/basic_trashcan.svg'
+import CancelIcon from '../icons/arrows_remove.svg';
 
-function MemoCard() {
+export const MemoCard = ({ editMode, onEdit, memoText, onSave, onCancel, onUpdate }) => {
+
     return (
         <div className="border border-grey p-2 w-2/5">
-            <textarea className="block border border-grey m-2 m-auto w-full"></textarea>
+            <textarea
+                className="block border border-grey m-2 m-auto w-full"
+                onFocus={ onEdit }
+                value={ memoText }
+                onInput={ event => onUpdate(event.target.value) }
+            />
+
             <div className="flex justify-around mt-2">
-                <button>
-                    <span>Save</span>
-                    <img className="w-4 ml-2" role="img" src={SaveIcon} alt=" "/>
-                </button>
-                <button>
-                    Edit
-                <img className="w-4 ml-2" role="img" src={EditIcon} alt=" "/>
-                </button>
-                <button>
-                    Delete
-                <img className="w-4 ml-2" role="img" src={DeleteIcon} alt=" "/>
-                </button>
+
+                {editMode ?
+                    <div className="ml-auto">
+                        <button className="mr-4" onClick={ onSave } >
+                            Save
+                            <img className="w-4 ml-2" role="img" src={ SaveIcon } alt=" " />
+                        </button>
+                        <button onClick={ onCancel } >
+                            Cancel
+                            <img className="w-4 ml-2" role="img" src={ CancelIcon } alt=" " />
+                        </button>
+                        <button>
+                            Delete
+                            <img className="w-4 ml-2" role="img" src={ DeleteIcon} alt=" " />
+                        </button>
+                    </div>
+                    : null}
+
             </div>
         </div>
     );
 }
-
-export default MemoCard;
