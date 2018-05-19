1a4b974f8fb89d1ac9a3aab40a5357997daa9570
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-05-19 16:15:20 +0200

    refactor and styling of exchangecard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Wrapper from './components/Wrapper';
 import Container from './components/Container';
-import ExchangeCard from './components/ExchangeCard';
+import { ExchangeCard } from './components/ExchangeCard';
 import TimerCard from './components/TimerCard';
 import { ClockCard } from './components/ClockCard';
 import { MemoCard } from './components/MemoCard';
--- src/components/ExchangeCard.js
+++ src/components/ExchangeCard.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import EuroIcon from '../icons/ecommerce_euro.svg';
 import RefreshIcon from '../icons/arrows_rotate.svg';
+import ArrowsIcon from '../icons/arrows_switch_horizontal.svg';
 
-function ExchangeCard() {
+export const ExchangeCard = () => {
     return (
-        <div className="border border-grey p-2 w-1/4">
-            <p>10 sek</p>
-            <p>
-                1 euro <img className="w-4" role="img" src={EuroIcon} alt=" " />
-            </p>
-            <button className="font-light font-sans">
-                Refresh
-                <img
-                    className="w-4 ml-2"
-                    role="img"
-                    src={RefreshIcon}
-                    alt=" "
-                />
-            </button>
+        <div className="border border-grey p-4 w-1/4 flex flex-col justify-between">
+            <div className="flex justify-center text-lg my-2">
+                <p>10 sek</p>
+                <img className="w-4 self-center mx-4" src={ArrowsIcon} alt="" />
+                <p>1 euro</p>
+            </div>
+            <div className="flex justify-between">
+                <p className="text-sm self-end">
+                    Updated: <time>09:09:1990</time>
+                </p>
+                <button className="font-light font-sans">
+                    Refresh
+                    <img
+                        className="w-4 ml-2"
+                        role="img"
+                        src={RefreshIcon}
+                        alt=" "
+                    />
+                </button>
+            </div>
         </div>
     );
-}
-
-export default ExchangeCard;
+};
