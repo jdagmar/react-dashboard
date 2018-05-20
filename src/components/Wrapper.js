b70bebf4d69382ec6df0dbaec8ca435fa0c36d08
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-05-20 19:02:36 +0200

    added max-width for main

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 
 export const Wrapper = ({ children }) => (
-    <main className="m-auto flex flex-wrap justify-around p-2 font-sans font-light">
-        {' '}
-        {children}{' '}
+    <main className="m-auto flex flex-wrap justify-around p-2 font-sans font-light max-w-2xl">
+        {children}
     </main>
 );
