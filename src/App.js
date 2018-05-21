d004d1b9edf0e92f246d719ab8e174cc8f06ec7c
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-05-21 17:49:57 +0200

    implemented timer function in timercard

--- src/App.js
+++ src/App.js
@@ -19,6 +19,55 @@ class App extends Component {
         clock: {
             date: new Date(),
         },
+        timer: {
+            timerStarted: false,
+            timerEnded: false,
+            interval: 0,
+            startInterval: 60,
+            counter: '',
+        },
+    };
+
+    handleSelectedOption = event => {
+        this.setState({
+            timer: { ...this.state.timer, startInterval: event.target.value },
+        });
+    };
+
+    handleTimerStart = () => {
+        const counter = setInterval(() => {
+            this.setState({
+                timer: {
+                    ...this.state.timer,
+                    interval: this.state.timer.interval - 1,
+                },
+            });
+
+            if (this.state.timer.interval === 0) {
+                clearInterval(this.state.timer.counter);
+            }
+        }, 1000);
+
+        this.setState({
+            timer: {
+                ...this.state.timer,
+                interval: this.state.timer.startInterval,
+                timerStarted: true,
+                counter,
+            },
+        });
+    };
+
+    handleTimerCancel = () => {
+        this.setState({
+            timer: { ...this.state.timer, timerStarted: false, interval: 0 },
+        });
+
+        clearInterval(this.state.timer.counter);
+    };
+
+    handleTimerReset = () => {
+        this.handleTimerStart();
     };
 
     handleMemoSave = () => {
@@ -55,7 +104,14 @@ class App extends Component {
                         <GetWeather />
                     </div>
                     <Container>
-                        <TimerCard />
+                        <TimerCard
+                            timerStarted={this.state.timer.timerStarted}
+                            resetTimer={this.handleTimerReset}
+                            startTimer={this.handleTimerStart}
+                            cancelTimer={this.handleTimerCancel}
+                            getOptionValue={this.handleSelectedOption}
+                            interval={this.state.timer.interval}
+                        />
                         <MemoCard
                             editMode={this.state.memo.editMode}
                             memoText={this.state.memo.memoText}
--- src/components/TimerCard.js
+++ src/components/TimerCard.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import TimerIcon from '../icons/basic_anticlockwise.svg';
 
-export const TimerCard = () => {
-    let timerStarted = true;
-    let timerEnded = false;
+const FormatedInterval = ({ interval }) => {
+    const minutes = Math.floor(interval / 60);
+    const seconds = interval - minutes * 60;
+
+    return (
+        <p className="text-center text-2xl my-4">
+            <time>{`0${minutes}: ${seconds < 10 ? 0 : ''}${seconds}`}</time>
+        </p>
+    );
+};
+
+export const TimerCard = ({
+    timerStarted,
+    startTimer,
+    cancelTimer,
+    getOptionValue,
+    interval,
+    resetTimer,
+}) => {
     return (
         <div className="shadow w-2/5 flex bg-white">
             <div className="bg-indigo-darker p-2">
@@ -12,49 +28,66 @@ export const TimerCard = () => {
             <div className="p-2 text-center w-full">
                 {timerStarted ? (
                     <React.Fragment>
-                        <select
-                            className="block m-auto w-full border border-grey-light"
-                            name="interval"
-                            size="6"
+                        <FormatedInterval interval={interval} />
+                        <button
+                            onClick={cancelTimer}
+                            className="block w-full border border-red-dark my-2 p-4 font-sans font-light hover:bg-red-dark hover:text-white"
                         >
-                            <option className="font-sans font-light" value="5">
-                                5 min
-                            </option>
-                            <option className="font-sans font-light" value="10">
-                                10 min
-                            </option>
-                            <option className="font-sans font-light" value="15">
-                                15 min
-                            </option>
-                            <option className="font-sans font-light" value="20">
-                                20 min
-                            </option>
-                            <option className="font-sans font-light" value="25">
-                                25 min
-                            </option>
-                            <option className="font-sans font-light" value="30">
-                                30 min
-                            </option>
-                        </select>
-                        <button className="block w-full my-2 border border-green p-4 font-sans font-light hover:bg-green hover:text-white">
-                            Start
-                        </button>
-                    </React.Fragment>
-                ) : (
-                    <React.Fragment>
-                        <p className="text-center text-2xl my-4">
-                            <time>00:00:00</time>
-                        </p>
-                        <button className="block w-full border border-red-dark my-2 p-4 font-sans font-light hover:bg-red-dark hover:text-white">
                             Cancel
                         </button>
-                        {timerEnded ? (
+                        {interval <= 0 ? (
                             <React.Fragment>
-                                <button className="block w-full border border-yellow-dark my-2 p-4 font-sans font-light hover:bg-yellow-dark hover:text-white">
+                                <button
+                                    onClick={resetTimer}
+                                    className="block w-full border border-yellow-dark my-2 p-4 font-sans font-light hover:bg-yellow-dark hover:text-white"
+                                >
                                     Reset
                                 </button>
                             </React.Fragment>
                         ) : null}
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                        <select
+                            onChange={getOptionValue}
+                            className="block m-auto w-full border border-grey-light"
+                            name="interval"
+                            size="5"
+                        >
+                            <option className="font-sans font-light" value={60}>
+                                1 min
+                            </option>
+                            <option
+                                className="font-sans font-light"
+                                value={60 * 2}
+                            >
+                                2 min
+                            </option>
+                            <option
+                                className="font-sans font-light"
+                                value={60 * 3}
+                            >
+                                3 min
+                            </option>
+                            <option
+                                className="font-sans font-light"
+                                value={60 * 4}
+                            >
+                                4 min
+                            </option>
+                            <option
+                                className="font-sans font-light"
+                                value={60 * 5}
+                            >
+                                5 min
+                            </option>
+                        </select>
+                        <button
+                            onClick={startTimer}
+                            className="block w-full my-2 border border-green p-4 font-sans font-light hover:bg-green hover:text-white"
+                        >
+                            Start
+                        </button>
                     </React.Fragment>
                 )}
             </div>
